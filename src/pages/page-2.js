b2ad5125eae82af8c453d84ba6c47e7a0595f200
taxiyoshida <taxi.yoshida@gmail.com>
2018-11-15 08:54:21 +0900

    1

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,6 +7,94 @@ const SecondPage = () => (
   <Layout>
     <h1>Indonesia Age</h1>
     <hr></hr>
+
+    <h3>Blog</h3>
+    <ul class="ul1">
+    <li class="li4"><a href="https://taxi-yoshida.hatenablog.com/entry/2017/01/07/210614">2010～2015年に私がインドネシアでやっていたこと</a> </li>
+    </ul>
+    <h3><strong>Media Publishing</strong></h3>
+    <ul class="ul2">
+    <li class="li8"><span class="s7">2014.7.22 </span><strong>『</strong><a href="http://d.hatena.ne.jp/keyword/%BD%B5%B4%A9%A5%A8%A5%B3%A5%CE%A5%DF%A5%B9%A5%C8"><span class="s2"><strong>週刊エコノミスト</strong></span></a><strong>』</strong><a href="http://www.slideshare.net/takushiyoshida/201472213p"><span class="s5">13</span></a><span class="s7">,<a href="http://www.slideshare.net/secret/h99NfipQlsLC06"><span class="s2">14,15P</span></a></span>「<a href="http://d.hatena.ne.jp/keyword/%A5%B8%A5%E7%A5%B3%A1%A6%A5%A6%A5%A3%A5%C9%A5%C9"><span class="s2">ジョコ・ウィドド</span></a>氏が勝利宣言、真の民主政時代の到来か」</li>
+    <li class="li4"><span class="s1">2014.11.4</span><span class="s4"><strong>『</strong><a href="http://d.hatena.ne.jp/keyword/%BD%B5%B4%A9%A5%A8%A5%B3%A5%CE%A5%DF%A5%B9%A5%C8"><span class="s2"><strong>週刊エコノミスト</strong></span></a><strong>』</strong><a href="http://www.slideshare.net/takushiyoshida/2014114-46612444"><span class="s2">「インドネシアは目覚めるか　新大統領が抱える</span><span class="s5">5</span><span class="s2">つの課題」</span></a></span></li>
+    </ul>
+    <h3>ASEAN/APEC Summit, 2013/10</h3>
+    <ul class="ul1">
+    <li class="li4"><span class="s4">APEC/ASEAN首脳会合。TPPをめぐる各国との折衝、国内調整に関して日本政府・APECを取材(「<a href="http://jakartashimbun.com/free/detail/13852.html"><span class="s2">「大筋合意」正念場</span> <span class="s2">分野、関税撤廃で難航</span> <span class="s2">ＴＰＰ閣僚会合終える</span> <span class="s2">甘利ＴＰＰ相「年内妥結共有」</span></a>)</span></li>
+    <li class="li3">南シナ海問題では中国の突出を米国、日本、インドなどを問題に引き込むことで勢力均衡策をとるASEANの状況を取材(<a href="http://www.jakartashimbun.com/free/detail/13914.html"><span class="s2">「主役」中国が矢面に</span> <span class="s2">南シナ海問題で　投資・貿易で補填狙う</span><span class="s5"> ASEAN</span><span class="s2">関連首脳会議開幕」　</span><span class="s5">(2013</span><span class="s2">年</span><span class="s5">10</span><span class="s2">月</span><span class="s5">10</span><span class="s2">日</span><span class="s5">)</span></a>） </li>
+    </ul>
+    <ul class="ul1">
+    <li class="li3">Coverage of<span class="Apple-converted-space">  </span>APEC / ASEAN Summit. Negotiations among APEC / ASEAN countries surrounding the TPP. Japan move on the regional economic partnership. (<span class="s6">「<a href="http://jakartashimbun.com/free/detail/13852.html"><span class="s2">「大筋合意」正念場</span> <span class="s2">分野、関税撤廃で難航</span> <span class="s2">ＴＰＰ閣僚会合終える</span> <span class="s2">甘利ＴＰＰ相「年内妥結共有」</span></a></span>)</li>
+    <li class="li3">South China Sea problem. ASEAN taking equilibrium measures toward China's protrusion with involving the United States, Japan, India in the situation.(<a href="http://www.jakartashimbun.com/free/detail/13914.html"><span class="s2">「主役」中国が矢面に</span> <span class="s2">南シナ海問題で　投資・貿易で補填狙う</span><span class="s5"> ASEAN</span><span class="s2">関連首脳会議開幕」　</span><span class="s5">(2013</span><span class="s2">年</span><span class="s5">10</span><span class="s2">月</span><span class="s5">10</span><span class="s2">日</span><span class="s5">)</span></a>）</li>
+    </ul>
+    <h3>Indonesia Presidential Election 2014</h3>
+    <ul>
+    <li>Joko Widodo、Prabowo Subiantoの両陣営に選挙戦当初から数年に渡り取材した。開発独裁派の石油輸入に絡む汚職疑惑をめぐるスクープ。「最も取材力のある外国人記者」。</li>
+    </ul>
+    <ul class="ul1">
+    <li class="li3">Coverage of two presidential candidate, Joko Widodo and Prabowo Subianto for several years. Scoping<span class="Apple-converted-space">  </span>corruption in<span class="Apple-converted-space">  </span>oil import and<span class="Apple-converted-space">  </span>gasoline subsidy by developmental authoritarianism group</li>
+    <li class="li3">“Foreign journalist with the best coverage capability”</li>
+    </ul>
+    <h3>Religious and race affairs related to presidential election</h3>
+    <p class="p8">インドネシアはムスリムマジョリティだが、イスラムのあり方は多様だ。大統領選挙ではムスリムの各勢力とプロテスタント、カトリックの宗教めぐり激しい中傷合戦が行われた。 </p>
+    <p class="p3">Indonesia is a Muslim Majority country, but Islam in Indonesia is varied. In the presidential election was a fierce battle against Muslims, Protestantism and Catholicism etc.</p>
+    <ul class="ul1">
+    <li class="li8"><span class="s7">最大民族ジャワ人のイスラム教総本山があるプサントレン（イスラム寄宿舎）を訪問、宿泊して取材した。プサントレンに対して行われた宗教差別キャンペーンの実態を調査(<a href="http://jakartashimbun.com/free/detail/18874.html"><span class="s3">「東ジャワ州大統領選挙情勢</span><span class="s2">(</span><span class="s3">上</span><span class="s2">)</span><span class="s3">キアイの支持争奪戦</span> <span class="s3">票田揺さぶる中傷タブロイド」</span></a>(2014</span>年<span class="s7">07</span>月<span class="s7">01</span>日<span class="s7">))</span></li>
+    <li class="li8">東ジャワ州情勢分析。人口集中地域であり、宗教と社会の距離が典型的な地域を調査<span class="s7">(<a href="http://jakartashimbun.com/free/detail/18865.html"><span class="s3">「東ジャワ州大統領選挙情勢</span><span class="s2">(</span><span class="s3">下</span><span class="s2">)</span><span class="s3">個人の自立か、キアイの権威か　からまるメディア、多様化する投票、土着の論理」</span></a>(2014</span>年<span class="s7">07</span>月<span class="s7">01</span>日<span class="s7">))</span></li>
+    <li class="li3">Visiting and stying at muslim school and largest<span class="Apple-converted-space">  </span>Javanese Islamic headquarters, Investigate the state of religious discrimination campaign toward them(<a href="http://jakartashimbun.com/free/detail/18874.html"><span class="s3">「東ジャワ州大統領選挙情勢</span><span class="s2">(</span><span class="s3">上</span><span class="s2">)</span><span class="s3">キアイの支持争奪戦</span> <span class="s3">票田揺さぶる中傷タブロイド」</span></a>(2014<span class="s6">年</span>07<span class="s6">月</span>01<span class="s6">日</span>))</li>
+    <li class="li5">Analyzing the situation in East Java province. Research areas with huge population, typical characteristics on religion and society(<a href="http://jakartashimbun.com/free/detail/18865.html"><span class="s3">「東ジャワ州大統領選挙情勢</span><span class="s2">(</span><span class="s3">下</span><span class="s2">)</span><span class="s3">個人の自立か、キアイの権威か　からまるメディア、多様化する投票、土着の論理」</span></a>(2014<span class="s6">年</span>07<span class="s6">月</span>01<span class="s6">日</span>))</li>
+    </ul>
+    <h3><strong>Mafia Migas(Oil Mafia) Scoop</strong></h3>
+    <p class="p8">石油輸入に関する汚職疑惑。北アフリカから輸入する低質石油の価格をシンガポールの取引所で嵩上げしたのち、国営企業がその嵩上げされた価格で買い取る。国営企業は補助金で石油価格を元の水準まで落とす。一連のプロセスにより補助金をシンガポールに移転できる。私は綿密な調査により汚職スキームの全容を明らかにした。補助金は当時の中央政府予算の25%程度に及んでいた。この汚職スキームに関する報道は海外メディアでは初めて。インドネシア関連のビジネスを行う外国人に大きなインパクトを与えた。</p>
+    <p class="p3">Investigative report on corruption allegation in oil imports. Some group inside government manipulate and raise the price of super-low-grade petroleum imported from North Africa at Singapore exchanges. They make state-owned enterprises buy it at irrationally raised price. Dropping the price to the original level with subsidies, corruption scheme can relocate subsidies to Singapore. The subsidy amounted to about 25% of the central government budget of Indonesia at that time. Coverage of this corruption allegation is the first publishing among oversea medias. This investigative report gave a great impact to foreigners conducting business related to Indonesia.</p>
+    <ul class="ul2">
+    <li class="li4"><span class="s9"><a href="http://jakartashimbun.com/free/detail/20179.html"><span class="s3">石油マフィア</span><span class="s5">(</span><span class="s3">上</span><span class="s5">)</span><span class="s3">「シンガポールのゴッドファーザー」</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/20199.html"><span class="s3">石油マフィア</span><span class="s5">(</span><span class="s3">中</span><span class="s5">)</span><span class="s3">「石油精製阻む高い壁　カレン社長辞職の背景」</span></a></span></li>
+    <li class="li3"><a href="http://jakartashimbun.com/free/detail/20213.html"><span class="s3">石油マフィア</span><span class="s5">(</span><span class="s3">下</span><span class="s5">)</span><span class="s3">「低品質石油ザタピ」</span></a></li>
+    </ul>
+    <h3><strong>Gasoline Subsidy</strong> </h3>
+    <p class="p8">上記の汚職疑惑の温床と考えられ、インドネシア財政の圧迫要因だったガソリン補助金に関する調査</p>
+    <p class="p3"><strong>Investigation on the gasoline subsidy which was considered as corruption allegation of “Oil Mafia”(Mafia Migas) and was a pressure factor of the Indonesian fiscal policy</strong></p>
+    <ul class="ul2">
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/11672.html"><span class="s3">補正予算案を可決</span> <span class="s3">燃料値上げへ</span> <span class="s3">貧困層補償も開始</span> <span class="s3">プレミウム６５００ルピアに</span><span class="s5">(2013</span><span class="s3">年</span><span class="s5">05</span><span class="s3">月</span><span class="s5">15</span><span class="s3">日</span><span class="s5">)</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="https://jakartashimbun.com/free/detail/10732.html"><span class="s3">貧困対策を拡充　燃料補助金政策の影響で　月内確定来月実施へ　コメ、教育、現金給付　</span><span class="s5">(2013</span><span class="s3">年</span><span class="s5">04</span><span class="s3">月</span><span class="s5">25</span><span class="s3">日</span><span class="s5">)</span></a></span></li>
+    </ul>
+    <h3>Biography of President Joko Widodo(2014~)</h3>
+    <p class="p8">ジョコウィ大統領の生涯を振り返るシリーズ。ビジネスマンから政治家へと転身する半生を物語化した。</p>
+    <p class="p3">A series looking back on President Joko Widodo’s life. Staying at Widodo’s hometown, Narrativeized the half-life that Widodo turned from businessman to politician</p>
+    <p class="p6"> </p>
+    <ul class="ul2">
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/20997.html"><span class="s3">【ジョコウィ物語】（</span><span class="s5">12</span><span class="s3">）政治へのいざない</span> <span class="s3">市長選、実業界が推す</span><span class="s5"> | </span><span class="s3">じゃかるた新聞</span> <span class="s3">インドネシアの日刊邦字新聞</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/21112.html"><span class="s3">【ジョコウィ物語】（</span><span class="s5">13</span><span class="s3">）</span> <span class="s3">風雲児、市庁舎に来る</span> <span class="s3">接戦しのぎ当選</span><span class="s5"> | </span><span class="s3">じゃかるた新聞</span> <span class="s3">インドネシアの日刊邦字新聞</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/21239.html"><span class="s3">【ジョコウィ物語】（</span><span class="s5">14</span><span class="s3">）不明朗さと断固闘う</span> <span class="s3">行政改革に着手</span><span class="s5"> | </span><span class="s3">じゃかるた新聞</span> <span class="s3">インドネシアの日刊邦字新聞</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/21392.html"><span class="s3">【ジョコウィ物語】（</span><span class="s5">15</span><span class="s3">）</span> <span class="s3">露天商移転に切り込む</span> <span class="s3">解決策は「ゾーニング」</span><span class="s5"> | </span><span class="s3">じゃかるた新聞</span> <span class="s3">インドネシアの日刊邦字新聞</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/21524.html"><span class="s3">【ジョコウィ物語】（</span><span class="s5">16</span><span class="s3">）移転の壁を越えた</span> <span class="s3">露店商の利を増やす</span><span class="s5"> | </span><span class="s3">じゃかるた新聞</span> <span class="s3">インドネシアの日刊邦字新聞</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/21652.html"><span class="s3">【ジョコウィ物語】（</span><span class="s5">17</span><span class="s3">）</span> <span class="s3">追風受け街づくり</span> <span class="s3">９割得票し再選</span><span class="s5"> | </span><span class="s3">じゃかるた新聞</span> <span class="s3">インドネシアの日刊邦字新聞</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/21800.html"><span class="s3">【ジョコウィ物語】（</span><span class="s5">18</span><span class="s3">）ソロが生んだ国民車</span> <span class="s3">中央への新たな「名刺」</span><span class="s5"> | </span><span class="s3">じゃかるた新聞</span> <span class="s3">インドネシアの日刊邦字新聞</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/21938.html"><span class="s3">【ジョコウィ物語】（</span><span class="s5">19</span><span class="s3">）</span> <span class="s3">当選、政治にうねり</span> <span class="s3">個人が政党連合倒す</span><span class="s5"> | </span><span class="s3">じゃかるた新聞</span> <span class="s3">インドネシアの日刊邦字新聞</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/22074.html"><span class="s3">【ジョコウィ物語】（</span><span class="s5">20</span><span class="s3">）千万首都に悪戦苦闘</span> <span class="s3">支持がうなぎ登り</span><span class="s5"> | </span><span class="s3">じゃかるた新聞</span> <span class="s3">インドネシアの日刊邦字新聞</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/22220.html"><span class="s3">【ジョコウィ物語】（</span><span class="s5">21</span><span class="s3">）</span> <span class="s3">いばらの道抜ける</span> <span class="s3">大統領選、僅差で制す</span><span class="s5"> | </span><span class="s3">じゃかるた新聞</span> <span class="s3">インドネシアの日刊邦字新聞</span></a></span><span class="s1"> </span></li>
+    </ul>
+    <h3> <strong>Poverty</strong></h3>
+    <ul class="ul2">
+    <li class="li4"><span class="s9"><a href="http://www.jakartashimbun.com/free/detail/11085.html"><span class="s3">移転先不足で暗礁</span> <span class="s3">プルイットの立ち退き</span> <span class="s3">州と貧困層が「正面衝突」</span><span class="s5"> (2013</span><span class="s3">年</span><span class="s5">05</span><span class="s3">月</span><span class="s5">15</span><span class="s3">日</span><span class="s5">)</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://jakartashimbun.com/free/detail/12776.html"><span class="s3">プルイット公園が完成</span> <span class="s3">市民の憩いの場に</span> <span class="s3">北ジャカルタ</span> <span class="s3">大洪水と立ち退きの跡地</span><span class="s5"> (2013</span><span class="s3">年</span><span class="s5">08</span><span class="s3">月</span><span class="s5">19</span><span class="s3">日</span><span class="s5">)</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://%20http//archive.fo/om92p"><span class="s3">「流入と密集の首都「スラム」再生計画」　</span><span class="s5">(2013</span><span class="s3">年</span><span class="s5">08</span><span class="s3">月</span><span class="s5">27</span><span class="s3">日</span><span class="s5">)</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://jakartashimbun.com/free/detail/19983.html"><span class="s3">水没した墓の上にたった集落、西ジャカルタ・カプック</span> <span class="s3">地盤沈下の波［上］</span><span class="s5">(2013</span><span class="s3">年</span><span class="s5">08</span><span class="s3">月</span><span class="s5">29</span><span class="s3">日</span><span class="s5">)</span></a></span></li>
+    <li class="li4"><span class="s9"><a href="http://d.hatena.ne.jp/keyword/%A5%B8%A5%E3%A5%AB%A5%EB%A5%BF"><span class="s3">ジャカルタ</span></a></span><span class="s4">の深刻な大気汚染。後に石油輸入<a href="http://d.hatena.ne.jp/keyword/%B1%F8%BF%A6"><span class="s2">汚職</span></a>で、低品質石油を輸入していることが要因のひとつとわかる。<a href="http://www.jakartashimbun.com/free/detail/18237.html"><span class="s2">【ジャカルタ・フォーカス】胸が苦しい、排気ガスの街</span> <span class="s2">深刻度増す大気汚染</span><span class="s5">(2014</span><span class="s2">年</span><span class="s5">05</span><span class="s2">月</span><span class="s5">31</span><span class="s2">日</span><span class="s5">) </span></a></span><span class="s1"><br /> </span></li>
+    </ul>
+    <h3><strong>Mafia</strong></h3>
+    <p class="p8">インドネシアではマフィアは政治に影響力をもっている。ヤクザは強烈な集票マシン。各種のビジネスにおいて必要不可欠な存在だ。私がインドネシアの政治経済分野において他を圧倒していた一つの要因は、このマフィア方面に通暁していたことだ。</p>
+    <p class="p3">In Indonesia, the mafia has influence on politics. Yakuza is an powerful vote collecting machine. It is essential for various business. One factor I was overwhelming the others in the political and economic field of Indonesia was that I was keenly aware of this mafia direction.</p>
+    <ul class="ul1">
+    <li class="li4"><span class="s4">【ジャカルタフォーカス】<a href="http://www.jakartashimbun.com/free/detail/12415.html"><span class="s2">「公と路上のルールぶつかる　タナアバンの露店撤去」</span></a></span><span class="s1">(2013</span><span class="s4">年</span><span class="s1">07</span><span class="s4">月</span><span class="s1">26</span><span class="s4">日</span><span class="s1">)</span></li>
+    <li class="li8">【変わるタナアバン】<span class="s7">(</span>上<span class="s7">)<a href="http://www.jakartashimbun.com/free/detail/13094.html"><span class="s3">「元露天商、路上への郷愁も」</span></a></span>　<span class="s7">(2013</span>年<span class="s7">09</span>月<span class="s7">05</span>日<span class="s7">)</span></li>
+    <li class="li8">【変わるタナアバン】<span class="s7">(</span>中<span class="s7">)<a href="http://www.jakartashimbun.com/free/detail/13127.html"><span class="s3">「再開発、目覚める」</span></a>(2013</span>年<span class="s7">09</span>月<span class="s7">06</span>日<span class="s7">)</span></li>
+    <li class="li8">【変わるタナアバン】<span class="s7">(</span>下<span class="s7">)<a href="http://www.jakartashimbun.com/free/detail/13145.html"><span class="s3">「山羊売りから王様へ　バン・ウチュ」</span></a>(2013</span>年<span class="s7">09</span>月<span class="s7">07</span>日<span class="s7">)</span></li>
+    <li class="li8">【ジャカルタフォーカス】<a href="http://www.jakartashimbun.com/free/detail/13127.html"><span class="s2">「線路横売春の終わり</span> <span class="s2">中央ジャカルタ・タナアバン」</span></a><span class="s7">(2014</span>年<span class="s7">08</span>月<span class="s7">09</span>日<span class="s7">)</span></li>
+    <li class="li4"><span class="s4">【ジャカルタフォーカス】<a href="http://www.jakartashimbun.com/free/detail/19618.html"><span class="s2">劣悪な売春現場働く女たちの声なき声</span> <span class="s2">「捨てられた街」で生きる</span> <span class="s2">合法化政策も必要か</span><span class="s5"> (2014</span><span class="s2">年</span><span class="s5">08</span><span class="s2">月</span><span class="s5">11</span><span class="s2">日</span><span class="s5">)</span></a></span></li>
+    </ul>
+
+    <hr></hr>
     <h1>Digital Marketing</h1>
       <ul>
         <li>記事のリンク</li>
